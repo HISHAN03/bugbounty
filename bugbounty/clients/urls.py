ca293078dfb474ff309b5c74a6bea8553328ca6d
HISHAN03 <moidinhishan@gmail.com>
2024-11-25 21:36:12 +0530

    small-updates-ui

--- bugbounty/clients/urls.py
+++ bugbounty/clients/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
      path('auth/',views.user_auth_page,name='user_auth'),
      path('dashboard/',views.user_dashboard,name='user_dashboard'),
      path('programs/<int:id>/', views.program_detail, name='program_detail'),
-     
-     # path('test/',views.signup,name='signup')
-     
 ]
